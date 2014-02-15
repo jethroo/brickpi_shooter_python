bc070603c79f11e60526739d06c862b7145eb4f5
Carsten Wirth <carsten.wirth@blacklane.com>
2014-02-16 00:17:39 +0100

    changed char conversion to python 2.7 and added brickpi imports and motro settings

--- brickpi_shooter.py
+++ brickpi_shooter.py
@@ -1,11 +1,16 @@
-# -*- coding: UTF-8 -*- 
+# -*- coding: UTF-8 -*-
 '''
 Created on 15.02.2014
 
 @author: jethroo
 '''
 ###
+
+#from BrickPi import * #import BrickPi.py file to use BricPi operations
 import time
+import threading
+import struct
+import sys
 
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the screen."""
@@ -15,7 +20,7 @@ class _Getch:
         except ImportError:
             self.impl = _GetchUnix()
 
-    def __call__(self): 
+    def __call__(self):
         bytes = self.impl()
         if bytes == b'\x03':
             raise KeyboardInterrupt
@@ -43,7 +48,7 @@ class _GetchUnix:
 
 
 class _GetchWindows:
-    def __init__(self): 
+    def __init__(self):
         import msvcrt
 
     def __call__(self):
@@ -53,26 +58,51 @@ class _GetchWindows:
 
 getch = _Getch()
 
+
+def turn_left():
+    print('turning left');
+    BrickPi.MotorSpeed[PORT_A] = 0
+    BrickPi.MotorSpeed[PORT_D] = 100
+    update_and_wait()
+
+def turn_right():
+    print('turning right');
+    BrickPi.MotorSpeed[PORT_A] = 0
+    BrickPi.MotorSpeed[PORT_D] = -100
+    update_and_wait()
+
+def shoot():
+    print('ready aim fire....');
+    BrickPi.MotorSpeed[PORT_A] = 400
+    BrickPi.MotorSpeed[PORT_D] = 0
+    update_and_wait()
+
+def update_and_wait():
+    BrickPiUpdateValues();
+    time.sleep(0.5)
+
 if __name__ == '__main__':
-    import sys
+
+    #BrickPiSetup() #setup the serial port for communication
+    #BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
+    #BrickPi.MotorEnable[PORT_D] = 1 #Enable the Motor D
+    #BrickPiSetupSensors() #Send the properties of sensors to BrickPi
+
     print ('Press a key')
     inkey = _Getch()
     while True:
         k=inkey()
         print(k[0])
-        # 'a' turn left
-        if (k[0] == 97):
-            print('turning left');
-            
-        # 'd' turn right
-        elif(k[0] == 100):
-            print('turning right');
-        
-        # 'space' to shoot
-        elif(k[0] == 32):
-            print('ready aim fire....');
-        
-        # 'x' for closing
-        elif(k[0] == 120):
+        # 'a' turn left ord: 97
+        if (k[0] == 'a'):
+            turn_left();
+        # 'd' turn right ord: 100
+        elif(k[0] == 'd'):
+            turn_right();
+        # 'space' to shoot ord: 32
+        elif(k[0] == ' '):
+            shoot();
+        # 'x' for closing ord: 120
+        elif(k[0] == 'x'):
             print('shooter is tired, shooter will sleep now, bye!');
             exit();
